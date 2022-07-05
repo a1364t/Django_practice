633b714d8562d608f9572b0957e1aca749610c39
Alireza <ali1985reza@gmail.com>
2022-07-05 18:42:59 +1000

    Models and serializers

--- myApp/models.py
+++ myApp/models.py
@@ -1,3 +1,16 @@
+from statistics import mode
 from django.db import models
 
-# Create your models here.
+
+class Student(models.Model):
+    first_name = models.CharField(max_length=25)
+    last_name = models.CharField(max_length=25)
+    std_number = models.IntegerField()
+
+
+    TEACHER = (
+        ("ahmadi", "Ahmadi"),
+        ("abdi", "Abdi"),
+        ("bahrami", "Bahrami")
+    )
+    teacher = models.CharField(max_length=7, choices = TEACHER)
--- myApp/views.py
+++ myApp/views.py
@@ -1,6 +1,7 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
+from .serializers import CalcSerializer
 
 
 @api_view()
@@ -16,33 +17,61 @@ def hello(request):
         return Response({"message": "Hello, {}".format(request.data["name"])})
 
 
+# @api_view(['POST'])
+# def calculator(request):
+#     try:
+#         num1 = request.data["num1"]
+#         num2 = request.data["num2"]
+#         opr = request.data["opr"]
+#     except:
+#         return Response({"error": "send num1 and num2 and opr"}, 
+#                         status=status.HTTP_400_BAD_REQUEST)
+#     else:
+#         if isinstance(num1, int) and isinstance(num2, int):
+
+#             if opr == "add":
+#                 return Response({"result": num1 + num2}, 
+#                                 status=status.HTTP_200_OK)
+#             elif opr == "sub":
+#                 return Response({"result": num1 - num2}, 
+#                                 status=status.HTTP_200_OK)
+#             elif opr == "div":
+#                 return Response({"result": num1 / num2}, 
+#                                 status=status.HTTP_200_OK)
+#             elif opr == "mul":
+#                 return Response({"result": num1 * num2}, 
+#                                 status=status.HTTP_200_OK)
+#             else:
+#                 return Response({"error": "send a valid opr"}, 
+#                         status=status.HTTP_400_BAD_REQUEST) 
+#         else:
+#             return Response({"error": "send integer values"}, 
+#                         status=status.HTTP_400_BAD_REQUEST)
+
+
 @api_view(['POST'])
 def calculator(request):
-    try:
-        num1 = request.data["num1"]
-        num2 = request.data["num2"]
-        opr = request.data["opr"]
-    except:
-        return Response({"error": "send num1 and num2 and opr"}, 
-                        status=status.HTTP_400_BAD_REQUEST)
-    else:
-        if isinstance(num1, int) and isinstance(num2, int):
+    ser = CalcSerializer(data=request.data)
 
-            if opr == "add":
+    if ser.is_valid():
+        num1 = ser.data['num1']
+        num2 = ser.data['num2']
+        opr = ser.data['opr']
+
+        if opr == "add":
                 return Response({"result": num1 + num2}, 
                                 status=status.HTTP_200_OK)
-            elif opr == "sub":
-                return Response({"result": num1 - num2}, 
-                                status=status.HTTP_200_OK)
-            elif opr == "div":
-                return Response({"result": num1 / num2}, 
-                                status=status.HTTP_200_OK)
-            elif opr == "mul":
-                return Response({"result": num1 * num2}, 
-                                status=status.HTTP_200_OK)
-            else:
-                return Response({"error": "send a valid opr"}, 
-                        status=status.HTTP_400_BAD_REQUEST) 
+        elif opr == "sub":
+            return Response({"result": num1 - num2}, 
+                            status=status.HTTP_200_OK)
+        elif opr == "div":
+            return Response({"result": num1 / num2}, 
+                            status=status.HTTP_200_OK)
+        elif opr == "mul":
+            return Response({"result": num1 * num2}, 
+                            status=status.HTTP_200_OK)
         else:
-            return Response({"error": "send integer values"}, 
-                        status=status.HTTP_400_BAD_REQUEST)
+            return Response({"error": "send a valid opr"}, 
+                    status=status.HTTP_400_BAD_REQUEST) 
+    else:
+        return Response(ser.errors, status=status.HTTP_400_BAD_REQUEST)
