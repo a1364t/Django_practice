13a7519fb6e085dc75ee02adf83e69dec5a76cba
Alireza <ali1985reza@gmail.com>
2022-07-07 12:41:07 +1000

    viewSets

--- car/urls.py
+++ car/urls.py
@@ -1,9 +1,16 @@
-from django.urls import URLPattern, path
+from django.urls import path
+from rest_framework.routers import DefaultRouter
 from car import views
 
 
+router = DefaultRouter()
+router.register('person', views.PersonViewSet)
+router.register('car', views.CarViewSet)
+
 urlpatterns = [
-    path('get-post-person', views.person_view),
-    path('get-post-car', views.car_view),
-    path('information', views.information_view)
+    #path('get-post-person', views.person_view),
+    #path('get-post-car', views.car_view),
+    #path('information', views.information_view)
 ]
+
+urlpatterns += router.urls
